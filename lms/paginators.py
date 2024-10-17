c47b86d89686f7aee1c828043103f7a32e6468d4
konstantin <kosta123139@gmail.com>
2024-10-17 17:00:41 +0300

    develop

--- lms/paginators.py
+++ lms/paginators.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class MyPageNumberPagination(PageNumberPagination):
-    page_size = ...
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = ...
+    max_page_size = 100
